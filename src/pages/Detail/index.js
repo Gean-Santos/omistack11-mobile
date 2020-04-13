495de8dfbc384aeee30b2b89cc0e2ea4bae4c0d3
Gean-Santos <geansantos_15@hotmail.com>
2020-04-13 15:33:54 -0300

    Add function email

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { View, TouchableOpacity, Image, Text } from 'react-native';
+import * as MailComposer from 'expo-mail-composer'
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './styles';
 
 export default function Detail() {
   const navigation = useNavigation();
+  const message = 'Olá APAE, estou entrando em contato pois goatsria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00 '
 
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['geansantos_15@hotmail.com'],
+      body: message,
+    })
+  }
+
+  function sendWhatsapp() {
+
   }
 
   return (
@@ -46,7 +60,7 @@ export default function Detail() {
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>
