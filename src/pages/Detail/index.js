8f1f44118da97b10614922020a7585af16168560
Gean-Santos <geansantos_15@hotmail.com>
2020-04-13 15:41:13 -0300

    add whatsapp function

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, TouchableOpacity, Image, Text } from 'react-native';
-import * as MailComposer from 'expo-mail-composer'
+import { View, TouchableOpacity, Image, Text, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -25,7 +25,7 @@ export default function Detail() {
   }
 
   function sendWhatsapp() {
-
+    Linking.openURL(`whatsapp://send?phone=5513997608250&text=${message}`)
   }
 
   return (
@@ -56,7 +56,7 @@ export default function Detail() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
